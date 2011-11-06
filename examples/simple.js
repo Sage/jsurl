52ec94a9c8d299bd6c9c22823cc8b84b59ec2d17
Bruno Jouhier <bjouhier@gmail.com>
2011-11-06 13:37:31 +0100

    added JSON+URL-encoding example to README

--- examples/simple.js
+++ examples/simple.js
@@ -2,6 +2,7 @@ var JSURL = require('jsurl');
 
 var obj = {name:"John Doe",age:42,children:["Mary","Bill"]};
 
-console.log("JSON =" + JSON.stringify(obj));
-console.log("JSURL=" + JSURL.stringify(obj));
-console.log("RTRIP=" + JSON.stringify(JSURL.parse(JSURL.stringify(obj))));
+console.log("JSON    =" + JSON.stringify(obj));
+console.log("JSURL   =" + JSURL.stringify(obj));
+console.log("JSON+URL=" + encodeURIComponent(JSON.stringify(obj)));
+console.log("RTRIP   =" + JSON.stringify(JSURL.parse(JSURL.stringify(obj))));
