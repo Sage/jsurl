24935492aadf70d63cb94b90df590f50a6ca7f46
Bruno Jouhier <bjouhier@gmail.com>
2012-12-26 16:58:45 +0100

    fixed #1 - NaN and infinities do not roundtrip

--- test/common/jsurlTest.js
+++ test/common/jsurlTest.js
@@ -6,11 +6,11 @@ var undefined;
 module('jsurl');
 
 function t(v, r) {
-	strictEqual(JSURL.stringify(v), r, "stringify " + JSON.stringify(v));
-	strictEqual(JSURL.stringify(JSURL.parse(JSURL.stringify(v))), r, "roundtrip " + JSON.stringify(v));
+	strictEqual(JSURL.stringify(v), r, "stringify " + (typeof v !== 'object' ? v : JSON.stringify(v)));
+	strictEqual(JSURL.stringify(JSURL.parse(JSURL.stringify(v))), r, "roundtrip " + (typeof v !== 'object' ? v : JSON.stringify(v)));
 }
 
-test('basic values', 20, function() {
+test('basic values', 26, function() {
 	t(undefined, undefined);
 	t(function() {
 		foo();
@@ -23,6 +23,10 @@ test('basic values', 20, function() {
 	t(-1.5, "~-1.5");
 	t("hello world\u203c", "~'hello*20world**203c");
 	t(" !\"#$%&'()*+,-./09:;<=>?@AZ[\\]^_`az{|}~", "~'*20*21*22*23!*25*26*27*28*29*2a*2b*2c-.*2f09*3a*3b*3c*3d*3e*3f*40AZ*5b*5c*5d*5e_*60az*7b*7c*7d*7e");
+	// JSON.stringify converts special numeric values to null
+	t(NaN, "~null");
+	t(Infinity, "~null");
+	t(-Infinity, "~null");
 });
 test('arrays', 4, function() {
 	t([], "~(~)");
