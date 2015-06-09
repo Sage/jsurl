0839a6853f6643f3c9f4487973f61bdcf7d7848b
Wout Mertens <Wout.Mertens@gmail.com>
2015-06-09 07:26:31 +0200

    Shorter encoding for true/false/null and some chars
    backwards compatible

--- test/common/jsurlTest.js
+++ test/common/jsurlTest.js
@@ -14,25 +14,25 @@ test('basic values', 26, function() {
 	t(function() {
 		foo();
 	}, undefined);
-	t(null, "~null");
-	t(false, "~false");
-	t(true, "~true");
+	t(null, "~!2");
+	t(false, "~!0");
+	t(true, "~!1");
 	t(0, "~0");
 	t(1, "~1");
 	t(-1.5, "~-1.5");
 	t("hello world\u203c", "~'hello*20world**203c");
-	t(" !\"#$%&'()*+,-./09:;<=>?@AZ[\\]^_`az{|}~", "~'*20*21*22*23!*25*26*27*28*29*2a*2b*2c-.*2f09*3a*3b*3c*3d*3e*3f*40AZ*5b*5c*5d*5e_*60az*7b*7c*7d*7e");
+	t(" !\"#$%&'()*+,-./09:;<=>?@AZ[\\]^_`az{|}~", "~'*20*21*22*23!*25*26'(*29*2a*2b*2c-.*2f09*3a*3b*3c*3d*3e*3f*40AZ*5b*5c*5d*5e_*60az*7b*7c*7d*7e");
 	// JSON.stringify converts special numeric values to null
-	t(NaN, "~null");
-	t(Infinity, "~null");
-	t(-Infinity, "~null");
+	t(NaN, "~!2");
+	t(Infinity, "~!2");
+	t(-Infinity, "~!2");
 });
 test('arrays', 4, function() {
 	t([], "~(~)");
 	t([undefined, function() {
 		foo();
 	},
-	null, false, 0, "hello world\u203c"], "~(~null~null~null~false~0~'hello*20world**203c)");
+	null, false, 0, "hello world\u203c"], "~(~!2~!2~!2~!0~0~'hello*20world**203c)");
 });
 test('objects', 4, function() {
 	t({}, "~()");
@@ -45,7 +45,7 @@ test('objects', 4, function() {
 		d: false,
 		e: 0,
 		f: "hello world\u203c"
-	}, "~(c~null~d~false~e~0~f~'hello*20world**203c)");
+	}, "~(c~!2~d~!0~e~0~f~'hello*20world**203c)");
 });
 test('mix', 2, function() {
 	t({
