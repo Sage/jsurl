ed7f6b646f11692107fabe976de794b59ac27516
Wout Mertens <Wout.Mertens@gmail.com>
2017-04-02 16:12:31 +0200

    Add `short` option and fix `true` in objects

--- test/jsurl2.js
+++ test/jsurl2.js
@@ -2,37 +2,43 @@ import test from 'ava'
 import {stringify, parse, tryParse} from '../lib/jsurl2'
 
 // test macro, both directions
-const cmp = (t, v, s) => {
+const cmp = (t, v, s, short) => {
 	// regular
 	t.is(stringify(v), s)
 	// roundtrip
 	t.is(stringify(parse(s)), s)
+	// short
+	t.is(stringify(v, {short: true}), short)
+	t.is(stringify(parse(short), {short: true}), short)
 }
 cmp.title = (title, v, s) => `${title} ${s}`
 
 // basic values
-test(cmp, undefined, '_U~')
-test(cmp, function () { foo(); }, '_U~')
-test(cmp, null, '_N~')
-test(cmp, false, '_F~')
-test(cmp, true, '~')
-test(cmp, 0, '0~')
-test(cmp, 1, '1~')
-test(cmp, -1.5, '-1.5~')
-test(cmp, 'hello world\u203c', 'hello_world\u203c~')
+test(cmp, undefined, '_U~', '_U')
+test(cmp, function () { foo(); }, '_U~', '_U')
+test(cmp, null, '_N~', '_N')
+test(cmp, false, '_F~', '_F')
+test(cmp, true, '~', '')
+test(cmp, 0, '0~', '0')
+test(cmp, 1, '1~', '1')
+test(cmp, -1.5, '-1.5~', '-1.5')
+test(cmp, '', '*~', '*')
+test(cmp, 'hello world\u203c', 'hello_world\u203c~', 'hello_world\u203c')
 test(cmp,
 	' !"#$%&\'()*+,-./09:;<=>?@AZ[\\]^_`az{|}~',
-	'*_!"#*S*.&*"*C*D***P,-./09:;<=>?@AZ[\\]^*_`az{|}*-~'
+	'*_!"#*S*.&*"*C*D***P,-./09:;<=>?@AZ[\\]^*_`az{|}*-~',
+	'*_!"#*S*.&*"*C*D***P,-./09:;<=>?@AZ[\\]^*_`az{|}*-'
 )
 // JSON.stringify converts special numeric values to null
-test(cmp, NaN, '_N~')
-test(cmp, Infinity, '_N~')
-test(cmp, -Infinity, '_N~')
-test(cmp, new Date(1456898746898), '_D2016-03-02T06:05:46.898Z~')
-test(cmp, new Date('2017-04-01'), '_D2017-04-01~')
+test(cmp, NaN, '_N~', '_N')
+test(cmp, Infinity, '_N~', '_N')
+test(cmp, -Infinity, '_N~', '_N')
+test(cmp, new Date(1456898746898), '_D2016-03-02T06:05:46.898Z~', '_D2016-03-02T06:05:46.898Z')
+test(cmp, new Date('2017-04-01'), '_D2017-04-01~', '_D2017-04-01')
 
 // arrays
-test(cmp, [], '!~')
+test(cmp, [], '!~', '!')
+test(cmp, [true], '!_T~', '!_T')
 test(cmp,
 	[
 		undefined, function () {
@@ -40,24 +46,30 @@ test(cmp,
 		},
 		null, false, 0, 'hello world\u203c'
 	],
-	'!_U~_U~_N~_F~0~hello_world\u203c~'
+	'!_U~_U~_N~_F~0~hello_world\u203c~',
+	'!_U~_U~_N~_F~0~hello_world\u203c'
 )
 
 // objects
-test(cmp, {}, '()~')
-test(cmp, {
-	a: undefined,
-	b: function () {
-		foo()
+test(cmp, {}, '()~', '(')
+test(cmp, {a: true, b: true, c: true}, '(a~~b~~c)~', '(a~~b~~c')
+test(cmp,
+	{
+		a: undefined,
+		b: function () {
+			foo()
+		},
+		c: null,
+		d: false,
+		t: true,
+		e: 0,
+		f: 'hello (world)\u203c'
 	},
-	c: null,
-	d: false,
-	t: true,
-	e: 0,
-	f: 'hello (world)\u203c'
-}, '(c~_N~d~_F~t~~e~0~f~hello_*Cworld*D\u203c)~')
-test(cmp, {"()": {}, c: {"~": "()"}}, '(*C*D~()c~(*-~**C*D))~')
-test(cmp, {a: [[[1]]]}, '(a~!!!1)~')
+	'(c~_N~d~_F~t~~e~0~f~hello_*Cworld*D\u203c)~',
+	'(c~_N~d~_F~t~~e~0~f~hello_*Cworld*D\u203c',
+)
+test(cmp, {"()": {}, c: {"~": "()"}}, '(*C*D~()c~(*-~**C*D))~', '(*C*D~()c~(*-~**C*D')
+test(cmp, {a: [[[1]]]}, '(a~!!!1)~', '(a~!!!1')
 // mix
 test(cmp, {
 	a: [
@@ -75,8 +87,8 @@ test(cmp, {
 		n: null
 	},
 	b: [],
-}, '(a~!!1~2~~!~_F~_T~()~c~(d~hello~e~()f~!~g~~n~_N)b~!)~')
-test(cmp, [[{a: [{b: [[1]]}]}]], '!!(a~!(b~!!1))~')
+}, '(a~!!1~2~~!~_F~_T~()~c~(d~hello~e~()f~!~g~~n~_N)b~!)~', '(a~!!1~2~~!~_F~_T~()~c~(d~hello~e~()f~!~g~~n~_N)b~!')
+test(cmp, [[{a: [{b: [[1]]}]}]], '!!(a~!(b~!!1))~', '!!(a~!(b~!!1')
 
 test('percent-escaped single quotes', t => {
 	t.deepEqual(parse('(a~*%27hello~b~*%27world~)~', {deURI: true}), {
@@ -95,8 +107,8 @@ test('percent-escaped percent-escaped single quotes', t => {
 test('tryParse', t => {
 	t.is(tryParse('_N~'), null)
 	t.is(tryParse('1~', 2), 1)
-	t.is(tryParse('1'), undefined)
-	t.is(tryParse('1', 0), 0)
+	t.is(tryParse('_'), undefined)
+	t.is(tryParse('_', 0), 0)
 })
 
 test('parse performance', t => {
